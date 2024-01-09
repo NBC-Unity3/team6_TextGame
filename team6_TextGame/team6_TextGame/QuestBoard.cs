0c0752b8eae3d7a4c5f73d9d3df820531a37dbf0
ruiroll <hyunbean67@gmail.com>
2024-01-09 16:06:57 +0900

    Create QuestBoard.cs
    Create QuestBoard class
    + load/save func

---
+++ team6_TextGame/team6_TextGame/QuestBoard.cs
@@ -0,0 +1,30 @@
+﻿using NBC_TextGame;
+using Newtonsoft.Json;
+
+namespace team6_TextGame
+{
+    internal class QuestBoard
+    {
+        public List<Quest> quests = new List<Quest>();
+
+        public void SaveOptions()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "/questboard.json";
+            string json = JsonConvert.SerializeObject(quests, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
+        public void LoadOptions()
+        {
+            string path = System.IO.Directory.GetCurrentDirectory() + "/questboard.json";
+            if (!File.Exists(path)) SaveOptions();
+
+            string json = File.ReadAllText(path);
+
+            List<Quest> quests = JsonConvert.DeserializeObject<List<Quest>>(json);
+
+            this.quests = quests;
+
+        }
+    }
+}
