9f8bda20c911a32366a7a0ce0a9eac14a895bca7
ruiroll <hyunbean67@gmail.com>
2024-01-09 19:21:56 +0900

    feat: QuestDetail
    퀘스트 디테일 화면을 보여주는 매서드 구현

--- team6_TextGame/team6_TextGame/Quest.cs
+++ team6_TextGame/team6_TextGame/Quest.cs
@@ -8,6 +8,8 @@
         public int achieve_count { get; set; } // 달성 횟수
         public int goal_count { get; set; } // 필요 달성 횟수
         public int gold_reward { get; set; } // 보상 골드
+        public bool isActive { get; set; } = false;
+        public bool isClear {  get; set; } = false;
         public List<EquipmentItem> item_rewards = new List<EquipmentItem>(); // 보상 아이템
 
     }
