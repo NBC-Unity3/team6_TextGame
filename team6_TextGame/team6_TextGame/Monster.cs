242134049947871d1cf6c5bf215269554756d028
KDHwan <litmu@naver.com>
2024-01-09 15:53:46 +0900

    feat: 몬스터 클래스 추가
    몬스터 클래스 추가
     - ShowState 상태 출력
     - IsDamaged 몬스터가 공격
     - IsAttack 플레이어가 공격

--- team6_TextGame/team6_TextGame/Monster.cs
+++ team6_TextGame/team6_TextGame/Monster.cs
@@ -1,4 +1,8 @@
 ﻿
+using NBC_TextGame;
+using System.Runtime.ConstrainedExecution;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace team6_TextGame
 {
     internal class Monster
@@ -8,6 +12,81 @@ namespace team6_TextGame
         public int atk { get; set; }    //공격력
         public int hp { get; set; }     //체력
         public int exp { get; set; }    //경험치 계수
-    
+
+
+        public void ShowState() // 현재 상태를 출력
+        {
+            if (hp > 0) 
+            { 
+                Console.WriteLine("Lv. " + level + " " + name + "  HP " + hp);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Lv. " + level + " " + name + " Dead ");
+                Console.ResetColor();
+            }
+        }
+
+        public void IsDamaged(Character character)    // 몬스터가 공격받는 함수
+        {
+            Random rand = new Random(); // 공격력의 10% 오차를 내기위한 랜덤 변수
+            int damage = rand.Next(9, 12);  // 9~11의 랜덤한 값에 0.1을 곱하는 방식으로 구함
+
+            Console.WriteLine(character.name + "의 공격!");
+            Console.WriteLine("Lv. " + level + " " + name + "을(를) 맞췄습니다. [데미지 : " + (int)((float)character.atk * (0.1 * (float)damage)) + "]");
+            Console.WriteLine("");
+            Console.WriteLine("Lv. " + level + " " + name);
+            Console.Write("HP " + hp + " -> ");
+            hp -= (int)((float)character.atk * (0.1 * (float)damage));
+            if (hp > 0)
+                Console.WriteLine("HP " + hp);  // 체력이 남아있으면 남은 체력을 출력
+            else
+                Console.WriteLine("Dead");    // 체력이 다 떨어지면 죽는 Dead 텍스트를 출력
+        }
+
+        public void IsAttack(Character character)   //몬스터가 공격하는 함수
+        {
+            if(hp > 0)
+            {
+                Console.WriteLine("Lv. " + level + " " + name + "의 공격!");
+                Console.WriteLine(character.name + "을(를) 맞췄습니다. [데미지 : " + atk + "]");
+                Console.WriteLine("");
+                Console.WriteLine("Lv. " + character.level + " " + character.name);
+                Console.Write("HP " + character.hp + " -> ");
+                character.hp -= atk;
+                if (hp > 0)
+                    Console.WriteLine("HP " + hp);  // 체력이 남아있으면 남은 체력을 출력
+                else
+                    Console.WriteLine("Dead");  // 체력이 다 떨어지면 죽는 Dead 텍스트를 출력
+            }
+        }
+    }
+
+    internal class Minion : Monster
+    {
+        public string name = "미니언";   //이름
+        public int level = 2;  //레벨
+        public int atk = 5;    //공격력
+        public int hp = 15;     //체력
+        public int exp = 1;    //경험치 계수
+    }
+
+    internal class CanonMinion : Monster
+    {
+        public string name = "대포 미니언";   //이름
+        public int level = 5;  //레벨
+        public int atk = 8;    //공격력
+        public int hp = 25;     //체력
+        public int exp = 5;    //경험치 계수
+    }
+
+    internal class VoidInsec : Monster
+    {
+        public string name = "공허충";   //이름
+        public int level = 3;  //레벨
+        public int atk = 9;    //공격력
+        public int hp = 10;     //체력
+        public int exp = 3;    //경험치 계수
     }
 }
