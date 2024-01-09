9e25689afb1bbb544d5e1879c20d5678a9fdea29
SeRDic0705 <serdic0705@gmail.com>
2024-01-09 16:26:09 +0900

    Create ConsumableItem.cs

---
+++ team6_TextGame/team6_TextGame/ConsumableItem.cs
@@ -0,0 +1,21 @@
+﻿using team6_TextGame;
+
+namespace team6_TextGame
+{
+    internal class ConsumableItem : Item
+    {
+        public string effect;
+        
+        public ConsumableItem(int id, string name, int atk, int def, int hp, string info, int price, string effect) : base(id, name, atk, def, hp, info, price)
+        {
+            this.id = id;
+            this.name = name;
+            this.atk = atk;
+            this.def = def;
+            this.hp = hp;
+            this.info = info;
+            this.price = price;
+            this.effect = effect;
+        }
+    }
+}
