907ee3999366377f5da58cd5d47636b902c06c62
황문규 <akazukinchacha0707@gmail.com>
2024-01-10 11:52:14 +0900

    feat: 몬스터가 등장할 수 있는 최소 던전 레벨 추가
    - 몬스터가 등장할 수 있는 최소 던전 레벨 추가

--- team6_TextGame/team6_TextGame/Monsters/VoidInsec.cs
+++ team6_TextGame/team6_TextGame/Monsters/VoidInsec.cs
@@ -4,6 +4,10 @@ namespace team6_TextGame.Monsters
 {
     internal class VoidInsec : Monster
     {
+        public static int GetMinimumLevel()
+        {
+            return 2; // 등장할 수 있는 최소 레벨
+        }
         public VoidInsec()
         {
             this.name = "공허충";   //이름
