66b47b5cf5a511a7c28b14dcb7ba2126cfb77dc6
황문규 <155706789+GeCrabKani@users.noreply.github.com>
2024-01-10 16:29:08 +0900

    chore: 숫자 색 변경 함수 기본 색 추가
    - 숫자 색 변경 함수 기본 색 설정
    -- 숫자 -> 빨강
    -- 이외 -> 하양

--- team6_TextGame/team6_TextGame/Program.cs
+++ team6_TextGame/team6_TextGame/Program.cs
@@ -433,7 +433,7 @@ class Program
         Console.ResetColor();
     }
     // 텍스트 내의 숫자만 다른 색으로 출력 (텍스트, 숫자 색, 텍스트 색)
-    public static void WriteColoredNumbers(string text, ConsoleColor numberColor, ConsoleColor textColor)
+    public static void WriteColoredNumbers(string text, ConsoleColor numberColor = ConsoleColor.Red, ConsoleColor textColor = ConsoleColor.White)
     {
         Console.ForegroundColor = textColor;
 
