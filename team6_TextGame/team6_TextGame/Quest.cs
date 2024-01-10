4cffd1c3a7a9f9fe11c989203247cf4db5d4c8a4
ruiroll <hyunbean67@gmail.com>
2024-01-10 19:57:43 +0900

    feat: add consumableItem as quest reward
    사용 아이템도 퀘스트 보상으로 받을 수 있도록 기능 추가

--- team6_TextGame/team6_TextGame/Quest.cs
+++ team6_TextGame/team6_TextGame/Quest.cs
@@ -1,4 +1,6 @@
-﻿namespace team6_TextGame
+﻿using team6_TextGame.ConsumableItem;
+
+namespace team6_TextGame
 {
     internal class Quest
     {
@@ -9,6 +11,7 @@
         public int goal_count { get; set; } // 필요 달성 횟수
         public int gold_reward { get; set; } // 보상 골드
         public List<EquipmentItem> item_rewards = new List<EquipmentItem>(); // 보상 아이템
+        public List<ConsumableItem.ConsumableItem> item_rewards2 = new List<ConsumableItem.ConsumableItem>();
         public bool isActive { get; set; } = false;
         public bool isClear { get; set; } = false;
 
@@ -21,7 +24,11 @@
             str += "보상\n";
             foreach (EquipmentItem item in item_rewards)
             {
-                str += item.name + "\n";
+                str += item.name + "x1\n";
+            }
+            foreach (ConsumableItem.ConsumableItem item in item_rewards2)
+            {
+                str += item.name + "x" + item.count + "\n";
             }
             str += gold_reward + "G\n";
 
