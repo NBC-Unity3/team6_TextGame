41ec6361400f1c825fb2336345ecae2adcb865a7
purangi <kaya0728@naver.com>
2024-01-10 13:35:53 +0900

    feat: Archer 스킬 구현

--- team6_TextGame/team6_TextGame/Character.cs
+++ team6_TextGame/team6_TextGame/Character.cs
@@ -91,6 +91,9 @@ namespace team6_TextGame
         {
             //단일기
             int damage = atk * 2;
+            mp -= 10;
+
+
             //monster.hp -= damage;
             //Console.WriteLine($"알파 스트라이크!\n{monster.name}에게 {damage}만큼의 대미지를 입혔습니다.");
             return damage;
@@ -100,6 +103,8 @@ namespace team6_TextGame
         {
             //광역기
             int damage = (int) Math.Round(atk * 1.2);
+            mp -= 15;
+
             //string names = "";
             //for(int i = 0; i < monster.Length; i++)
             //{
@@ -130,13 +135,35 @@ namespace team6_TextGame
 
         public override int Skill1(Monster monster)
         {
-            int damage = atk;
+            //단일기, 명중 부위 따라 데미지가 다른 설정
+            Random rand = new Random();
+
+            mp -= 15;
+
+            int part = rand.Next(1, 3);
+
+            int damage = 0;
+
+            switch (part)
+            {
+                case 1: //급소 관통
+                    damage = atk * 3;
+                    break;
+                case 2: //주요 팔다리 명중
+                    damage = (int)Math.Round(atk * 1.2);
+                    break;
+                case 3: //빗맞음
+                    damage = (int)Math.Round(atk * 0.8);
+                    break;
+            }
             return damage;
         }
 
         public virtual int Skill2(Monster[] monster)
         {
-            //광역기
+            mp -= 5; //자주 사용 가능하게, 하지만 위력 안높음
+
+            //광역기, 불화살로 본대 전체 타격
             int damage = atk;
             return damage;
         }
@@ -159,7 +186,12 @@ namespace team6_TextGame
 
         public override int Skill1(Monster monster)
         {
-            int damage = atk;
+            //속성 단일 공격 -> 몬스터마다 다르게 설정하고 싶은데 몬스터 받아오는 형태로 여기서 공격할지
+            //                  아니면 그냥 진짜 데미지값만 리턴할지 모르겠음
+            int damage = 0;
+
+            Random rand = new Random();
+
             return damage;
         }
 
