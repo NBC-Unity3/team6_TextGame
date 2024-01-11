ef011ea7449a9e9c6a2ca3ea9a87beef010981cd
purangi <kaya0728@naver.com>
2024-01-11 17:24:53 +0900

    chore: 마을 소개 문구 수정

--- team6_TextGame/team6_TextGame/Program.cs
+++ team6_TextGame/team6_TextGame/Program.cs
@@ -141,8 +141,8 @@ class Program
         while (true)
         {
             Console.Clear();
-            ui.TextColor("인벤토리", ConsoleColor.Yellow);
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+            ui.TextColor("마을", ConsoleColor.Yellow);
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
             ui.DrawLine();
 
             switch (ui.SelectList(new List<string>(new string[] { "1.상태보기", "2.전투 시작(현재 진행 :"+ dungeon.level +"층", "3.인벤토리", "4.상점", "5.퀘스트", "6.저장" })))
