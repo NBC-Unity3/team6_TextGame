a7b87e8632b6d5db9fa04fa39f27c116f474eef3
SeRDic0705 <serdic0705@gmail.com>
2024-01-11 17:38:34 +0900

    fix: Monsters

--- team6_TextGame/team6_TextGame/Characters/Monster.cs
+++ team6_TextGame/team6_TextGame/Characters/Monster.cs
@@ -2,10 +2,7 @@
 {
     internal class Monster : Character
     {
-        public string name { get; set; }   //이름
-        public int level { get; set; }  //레벨
-        public int atk { get; set; }    //공격력
-        public int hp { get; set; }     //체력
+
         public int exp { get; set; }    //경험치 계수
 
 
